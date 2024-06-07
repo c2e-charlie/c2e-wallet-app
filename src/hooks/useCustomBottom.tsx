e72d41e00ae5beab681b3a61b036bdc6c485ec96
chainwith <hellosjworld@gmail.com>
2024-06-07 10:40:29 +0900

    fix type issue

--- src/hooks/useCustomBottom.tsx
+++ src/hooks/useCustomBottom.tsx
@@ -1,19 +1,24 @@
-import {useRef} from 'react';
-import {Animated} from 'react-native';
+import { Route, NavigationState } from '@react-navigation/native';
+import { useRef } from 'react';
+import { Animated } from 'react-native';
 
-export const useCustomBottom = state => {
+interface CustomBottomState extends NavigationState {
+  routes: Route<string>[];
+}
+
+export const useCustomBottom = (state: CustomBottomState) => {
   const animatedValues = state.routes.map(
     () => useRef(new Animated.Value(1)).current,
   );
 
-  const handlePressIn = index => {
+  const handlePressIn = (index: number) => {
     Animated.spring(animatedValues[index], {
       toValue: 0.8,
       useNativeDriver: true,
     }).start();
   };
 
-  const handlePressOut = index => {
+  const handlePressOut = (index: number) => {
     Animated.spring(animatedValues[index], {
       toValue: 1,
       friction: 3,

5dc8bd5584ea3c2311ae9103fe2492bdba912668
c2e-charlie <charlie@c2eprotocol.com>
2024-06-10 16:47:29 +0900

    feat: babel 설정

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,24 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          '@': './src',
+          public: './public',
+        },
+      },
+    ],
+    // 추가된 플러그인들
+    [
+      '@babel/plugin-transform-private-methods',
+      { loose: true }, // 모두 동일한 'loose' 모드로 설정
+    ],
+    [
+      '@babel/plugin-transform-private-property-in-object',
+      { loose: true }, // 모두 동일한 'loose' 모드로 설정
+    ],
+  ],
 };
--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,16 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+const path = require('path');
 
-/**
- * Metro configuration
- * https://reactnative.dev/docs/metro
- *
- * @type {import('metro-config').MetroConfig}
- */
-const config = {};
+const defaultConfig = getDefaultConfig(__dirname);
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+const config = {
+  resolver: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      public: path.resolve(__dirname, 'public'),
+    },
+    ...defaultConfig.resolver,
+  },
+};
+
+module.exports = mergeConfig(defaultConfig, config);

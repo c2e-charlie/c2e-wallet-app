93e429246067acc8317ef8b499d7edd88e112013
chainwith <hellosjworld@gmail.com>
2024-06-13 15:11:24 +0900

    splash screen testImg

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StatusBar, useColorScheme, View } from 'react-native';
 import 'react-native-gesture-handler';
 import RootNavigator from './navigations/Stack';
 import { Colors } from 'react-native/Libraries/NewAppScreen';
 import { ThemeProvider } from 'styled-components/native';
 import theme from './theme';
+import SplashScreen from 'react-native-splash-screen';
 
 function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
@@ -13,6 +14,10 @@ function App(): React.JSX.Element {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
     flex: 1,
   };
+
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
 
   return (
     <ThemeProvider theme={theme}>

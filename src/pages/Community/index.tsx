bcb981a4356c54da3f3542c9df7db6db47aaa683
KuHell <itkdgus489@gmail.com>
2024-06-14 15:08:40 +0900

    feat: bottomsheet component 제작

--- src/pages/Community/index.tsx
+++ src/pages/Community/index.tsx
@@ -1,11 +1,14 @@
 import React, { useRef } from 'react';
 import { View, Text, Button } from 'react-native';
+import Modal, { modalStateAtom } from '@/components/Modal';
+import { useAtom } from 'jotai';
 import BottomSheetComponent from '@/components/BottomSheet';
 import { handlePresentModalPress } from '@/components/BottomSheet/index.utils';
 import { BottomSheetModal } from '@gorhom/bottom-sheet';
 
 const Community = () => {
   const bottomSheetModalRef = useRef<BottomSheetModal>(null);
+  const [isModalState, setIsModalState] = useAtom(modalStateAtom);
 
   const handleModalPress = handlePresentModalPress(bottomSheetModalRef);
 
@@ -20,8 +23,13 @@ const Community = () => {
       <Text>Community</Text>
       <Button title={'Open Bottom Sheet'} onPress={handleModalPress} />
       <BottomSheetComponent ref={bottomSheetModalRef}>
-        <Text>버디버디</Text>
+        <Button onPress={() => setIsModalState(true)} title={'모달 버튼'} />
       </BottomSheetComponent>
+      {isModalState && (
+        <Modal isModalState={isModalState} setIsModalState={setIsModalState}>
+          <Text> 모달입니다. </Text>
+        </Modal>
+      )}
     </View>
   );
 };
